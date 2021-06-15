0b8dbab2370b9a186da53661e7a6f027e13a971e
Hackmaster3120 <81357992+Hackmaster3120@users.noreply.github.com>
2021-06-15 15:45:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var firstName= "random";
+var secondName= "website";
+var a=10;
+var b=5;
+var thethinngamabob=a+b
+function clicked(){
+    console.log(firstName.concat(secondName))
+    console.log(thethinngamabob)
+}
